b4cddbc04baf22fc0a4ec1512dcf756592e217c6
murali1125 <murali1125s@gmail.com>
2020-03-21 23:39:53 +0530

    uc8

--- airthematic.sh
+++ airthematic.sh
@@ -7,7 +7,8 @@
 # compute  a%b+c
 # Store the results in a dictonary for every computation
 # read the values from dictionary into the array
-#------------------------------------------------------------------------------------
+# Sort the results to show the compuation result in the Desendiung order
+#-----------------------------------------------------------------------
 
 
 read -p " enter 'a  b  c 'values "  a b c
@@ -20,4 +21,28 @@ dictonary[operation3]=$(( c + a / b ))
 dictonary[operation4]=$(( a % b + c ))
 # read the dictionary values into array
 array=( ${dictonary[@]} )
+
+# function sort in decending order
+function decending(){
+	array=( "$@" )
+	# declaring a temporary variable and initial with array[0] value
+	temp=$(( array[0] ))
+	length=${#array[@]}
+	for (( i=0;i<$(( length -1 )); i++ ))
+	do
+		for (( j=$(( i + 1 )); j<$length;j++ ))
+		do
+			# if the element in j index is greater then element in index i, then swap elements
+			if [ ${array[j]} -gt ${array[i]} ]
+			then
+				temp=${array[i]}
+				array[i]=${array[j]}
+				array[j]=$temp
+			fi
+		done
+	done
+	echo "Decending order = ${array[@]}"
+}
+
+decending ${dictonary[@]}
 
