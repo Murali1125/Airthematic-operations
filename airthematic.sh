853912eb5616913bc9da45aa7e7e5fdd1e8e3d9f
murali1125 <murali1125s@gmail.com>
2020-03-21 23:16:12 +0530

    uc3

--- airthematic.sh
+++ airthematic.sh
@@ -1,7 +1,8 @@
 #! /bin/bash  -x
 
 # Write a program to take three inputs a, b & c
-# compute  a+b*c 
+# compute  a+b*c
+# compute  a*b+c
 #------------------------------------------------------------------------------------
 
 
@@ -10,4 +11,5 @@ read -p " enter 'a  b  c 'values "  a b c
 declare -A dictonary
 #compute the operations and store into variables
 dictonary[operation1]=$(( a + b * c ))
+dictonary[operation2]=$(( a * b + c ))
 
