3ad30e527138ce85e1598fb4f645576a104957f0
murali1125 <murali1125s@gmail.com>
2020-03-21 23:06:49 +0530

    uc1

--- airthematic.sh
+++ airthematic.sh
@@ -1,1 +1,9 @@
 #! /bin/bash  -x
+
+# Write a program to take three inputs a, b & c and  compute the following operations
+
+#------------------------------------------------------------------------------------
+
+
+read -p " enter 'a  b  c 'values "  a b c
+
